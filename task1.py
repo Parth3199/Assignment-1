954f3b4ec650db6224cbe13263e95fb8fca2a79e
Parth3199 <105925272+Parth3199@users.noreply.github.com>
2025-09-23 23:00:49 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,3 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+print("Addition: ", a + b,"\nSubtraction: ", a - b, "\nMultiplication: ", a*b, "\nDivision: ", a/b)
---
+++ task2.py
@@ -0,0 +1,3 @@
+f_name = str(input("Enter your first name: "))
+l_name = str(input("Enter your last name: "))
+print("Hi, ", f_name ,l_name,'!', ",Have a nice day!")
